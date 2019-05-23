18d8ec686942a76a95f9f3775d56ceac42da617e
Farnell Newton and Jaime Takikawa <farnell@>
2019-05-22 20:13:03 -0700

    Made images show and hide with toggling of button but not making them hide on load

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,29 @@
 $(document).ready(function() {
+  $("button.cats").click(function() {
+    $("ul#user").append("<li>Ah, you said cats!</li>");
+    $("ul#webpage").append("<li>Meow!</li>");
+  });
+  $("button.dogs").click(function() {
+    $("ul#user").append("<li>Ah, you said dogs!</li>");
+    $("ul#webpage").append("<li>Woof!</li>");
+  });
+  $("button.both").click(function() {
+    $("ul#user").append("<li>Ah, you like cats and dogs!</li>");
+    $("ul#webpage").append("<li>Meow!, Ruff!</li>");
+  });
   $("button#cats").click(function() {
-    $("ul#user").prepend("<li>Ah, you said cats!</li>");
-     $("ul#webpage").prepend("<li>Meow!</li>");
+    $("#img-cats").removeClass();
+    $("#img-cats").toggle();
+    $("#img-showing").toggle();
   });
   $("button#dogs").click(function() {
-    $("ul#user").prepend("<li>Ah, you said dogs!</li>");
-     $("ul#webpage").prepend("<li>Woof!</li>");
+    $("#img-dogs").removeClass();
+    $("#img-dogs").toggle();
+    $("#img-showing").toggle();
   });
   $("button#both").click(function() {
-    $("ul#user").prepend("<li>Ah, you like cats and dogs!</li>");
-     $("ul#webpage").prepend("<li>Meow!, Ruff!</li>");
+    $("#img-both").removeClass();
+    $("#img-both").toggle();
+    $("#img-showing").toggle();
   });
 });
